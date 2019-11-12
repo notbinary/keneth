24214fbcca05731b1e980f8d02e6a932c8186138
David Carboni <david@carboni.io>
2019-11-12 13:45:24 +0000

    Updating heroku.

--- app.py
+++ app.py
@@ -20,9 +20,12 @@ def files():
 @app.route('/check', methods = ['POST', 'GET'])
 def get_data():
 
-    vrn = request.form.get('vrn')
-    if not vrn:
+    vrm = request.form.get('vrm')
+    if not vrm:
         abort(403)
+
+    reg = vrm[:-3] + " " + vrm[-3:]
+    reg = reg.upper()
 
     api_token = "DTeXRL3AQn3hTbxKZdiNk4SP6Nih3GRwagMwv3d6"
     api_url = "https://uat.driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles"
@@ -30,17 +33,18 @@ def get_data():
         'Content-Type': 'application/json',
         'x-api-key': api_token
         }
-    body = {"registrationNumber":vrn}
+    body = {"registrationNumber":vrm}
 
     response = requests.post(api_url, headers=headers, json=body)
 
     response_body = response.content.decode('utf-8')
     if response.status_code == 200:
-        print(json.dumps(json.loads(response_body)))
-        return render_template('car.html', vrn=vrn)
+        details = json.loads(response_body)
+        print(json.dumps(details))
+        return render_template('car.html', details=details, reg=reg)
         #return jsonify(json.loads(response_body))
     else:
-        return f"That didn't work ({vrn}): {response.status_code}: {response_body}"
+        return f"That didn't work ({vrm}): {response.status_code}: {response_body}"
 
     # p = path if path else 'index.html'
     # return send_from_directory('static', p)
